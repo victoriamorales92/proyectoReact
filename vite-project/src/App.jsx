c4b7576448f27d7f78c67eca437d6551433f1d86
Vico <anavictoriamoralesperuzzi@gmail.com>
2024-10-23 17:38:50 -0300

    git push

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+import NavBar from "./components/NavBar/NavBar"
+
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <ItemListContainer />
+    </>
+  )
+}
+
+export default App
